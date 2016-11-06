130236c0810e7ebb4b9869ab98a9f7a753ab9cd7
Anant Prasad <anantprsd5@gmail.com>
2016-11-06 15:44:23 +0530

    App Tray

--- app/src/main/java/com/testapp/anantprsd5/testapp/MainActivity.java
+++ app/src/main/java/com/testapp/anantprsd5/testapp/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -18,33 +19,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void onBtnClicked(View v)
-    {
-        switch (v.getId()) {
-            case R.id.button:
-                Toast.makeText(MainActivity.this, "This launches Poplular movies", Toast.LENGTH_LONG).show();
-                break;
-            case R.id.button1:
-                Toast.makeText(MainActivity.this, "This launches Stock Hawk", Toast.LENGTH_LONG).show();
-                break;
-            case R.id.button2:
-                Toast.makeText(MainActivity.this, "This launches Build it bigger", Toast.LENGTH_LONG).show();
-                break;
-            case R.id.button3:
-                Toast.makeText(MainActivity.this, "This launches Go ubiquitous", Toast.LENGTH_LONG).show();
-                break;
-            case R.id.button4:
-                Toast.makeText(MainActivity.this, "This launches Make your app material", Toast.LENGTH_LONG).show();
-                break;
-            case R.id.button5:
-                Toast.makeText(MainActivity.this, "This launches Capstone", Toast.LENGTH_LONG).show();
-                break;
-
-        }
+    public void onBtnClicked(View v) {
+        Button button = (Button) v;
+        MessageBox("This will open " + button.getText().toString());
     }
 
-    public void MessageBox(String message)
-    {
+    public void MessageBox(String message){
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 }
